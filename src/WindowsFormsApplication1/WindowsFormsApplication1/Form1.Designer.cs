31875ccaa23f4906a6be639985dcc5c3a0887af5
adonel <adonel@gmail.com>
2015-10-10 15:34:50 +0300

    Fixed exceptions Added support for starting/stoping the listener GUI enhancements Stack-based recognition of letters Various fixes

--- src/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ src/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,21 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Lime;
+            this.btnStart.Location = new System.Drawing.Point(21, 198);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(181, 198);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(181, 227);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(235, 164);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(21, 183);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GSLT";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- src/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ src/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -7,19 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Leap;
+using LeapGSLT;
 
 namespace WindowsFormsApplication1
-{
+{        
     public partial class Form1 : Form
     {
+        public static LeapListener leap_listener;
+        public static Controller leap_controller;
+
         public Form1()
         {
             InitializeComponent();
+            initializeLeapListener();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            lblStatus.Text = "Paused";
+            leap_controller.RemoveListener(leap_listener);
         }
-    }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Waiting for user input...";
+            leap_controller.AddListener(leap_listener);
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Paused";
+        }
+
+        private void initializeLeapListener()
+        {
+            // Create a listener and controller
+            leap_listener = new LeapListener();
+            leap_controller = new Controller();
+
+            // Have the listener receive events from the controller
+            leap_controller.AddListener(leap_listener);
+        }
+        
+    }    
 }
