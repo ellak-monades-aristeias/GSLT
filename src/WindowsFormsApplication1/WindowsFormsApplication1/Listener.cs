c56b35a7eb47e0735d58a2cd334ba402b522529a
adonel <adonel@gmail.com>
2015-10-13 00:33:20 +0300

    1. Various changes in letter recognition stack 2. Improved speed based on stack push/pop optimization

--- src/WindowsFormsApplication1/WindowsFormsApplication1/Listener.cs
+++ src/WindowsFormsApplication1/WindowsFormsApplication1/Listener.cs
@@ -1,24 +1,39 @@
 using System;
 using System.Threading;
+using System.Collections;
 using Leap;
+using System.Collections.Generic;
 
 namespace LeapGSLT
 {
     public class LeapListener : Listener
     {
-        private Object thisLock = new Object();
+        private Stack gesture_stack; //this stack keeps the gesture to identify potential commands for starting/stoping the program
+        private Stack letter_stack;
 
         private void SafeWriteLine(String line)
         {
-            lock (thisLock)
+            Console.WriteLine(line);     
+        }
+
+        public char getLastLetter()
+        {
+            if (letter_stack.Count > 0)
             {
-                Console.WriteLine(line);
+                return (char)letter_stack.Peek();
+            }
+            else
+            {
+                char c = ' ';
+                return c; //no letter identified
             }
         }
 
         public override void OnInit(Controller controller)
         {
             SafeWriteLine("Initialized");
+
+            gesture_stack.Clear();
         }
 
         public override void OnConnect(Controller controller)
@@ -72,6 +87,65 @@ namespace LeapGSLT
                             + ", wrist position: " + arm.WristPosition
                             + ", elbow position: " + arm.ElbowPosition);
 
+                float pitch = hand.Direction.Pitch;
+                float yaw = hand.Direction.Yaw;
+                float roll = hand.PalmNormal.Roll;
+
+                //Here, based on the identified Hand and Finger information, we can easily identify all the distinct letters
+                Stack candidate_stack = new Stack();
+                if (pitch == 0.0 && yaw == 0.0)
+                {
+                    //candidates are Á, Â, Ä, Ç, É, Ê, Ï, Ñ, Õ, Ö, ×, Ø
+                    List<char> current_list = new List<char>();
+                    current_list.Add('A');
+                    current_list.Add('Â');
+                    current_list.Add('Ä');
+                    current_list.Add('Ç');
+                    current_list.Add('É');
+                    current_list.Add('Ê');
+                    current_list.Add('Ï');
+                    current_list.Add('Ñ');
+                    current_list.Add('Õ');
+                    current_list.Add('Ö');
+                    current_list.Add('×');
+                    current_list.Add('Ø');
+
+                    candidate_stack.Push(current_list);
+                }
+                else if (pitch == 90.0 && yaw == 0.0)
+                {
+                    //candidates are Ã, Ë, Ì, Í, Ð, Ó, Ô
+                    List<char> current_list = new List<char>();
+                    current_list.Add('Ã');
+                    current_list.Add('Ë');
+                    current_list.Add('Ì');
+                    current_list.Add('Í');
+                    current_list.Add('Ð');
+                    current_list.Add('Ó');
+                    current_list.Add('Ô');                    
+
+                    candidate_stack.Push(current_list);
+                }
+                else if (pitch == 0.0 && yaw == 90.0)
+                {
+                    //candidates are Å, 
+                    List<char> current_list = new List<char>();
+                    current_list.Add('Å');                    
+                    
+                    candidate_stack.Push(current_list);
+                }
+                else if (pitch == 0.0 && yaw == -90.0)
+                {
+                    //candidates are Æ, È, Î, Ù
+                    List<char> current_list = new List<char>();
+                    current_list.Add('Æ');
+                    current_list.Add('È');
+                    current_list.Add('Î');
+                    current_list.Add('Ù');                    
+
+                    candidate_stack.Push(current_list);
+                }
+
                 // Get fingers
                 foreach (Finger finger in hand.Fingers)
                 {
@@ -92,7 +166,8 @@ namespace LeapGSLT
                     }
                 }
 
-            }
+
+            }            
 
             // Get tools
             foreach (Tool tool in frame.Tools)
@@ -140,6 +215,9 @@ namespace LeapGSLT
                                        + ", radius: " + circle.Radius
                                        + ", angle: " + sweptAngle
                                        + ", " + clockwiseness);
+
+                        gesture_stack.Push(circle);
+
                         break;
                     case Gesture.GestureType.TYPE_SWIPE:
                         SwipeGesture swipe = new SwipeGesture(gesture);
@@ -148,6 +226,7 @@ namespace LeapGSLT
                                        + ", position: " + swipe.Position
                                        + ", direction: " + swipe.Direction
                                        + ", speed: " + swipe.Speed);
+                        gesture_stack.Push(swipe);
                         break;
                     case Gesture.GestureType.TYPE_KEY_TAP:
                         KeyTapGesture keytap = new KeyTapGesture(gesture);
@@ -155,6 +234,7 @@ namespace LeapGSLT
                                        + ", " + keytap.State
                                        + ", position: " + keytap.Position
                                        + ", direction: " + keytap.Direction);
+                        gesture_stack.Push(keytap);
                         break;
                     case Gesture.GestureType.TYPE_SCREEN_TAP:
                         ScreenTapGesture screentap = new ScreenTapGesture(gesture);
@@ -162,6 +242,7 @@ namespace LeapGSLT
                                        + ", " + screentap.State
                                        + ", position: " + screentap.Position
                                        + ", direction: " + screentap.Direction);
+                        gesture_stack.Push(screentap);
                         break;
                     default:
                         SafeWriteLine("  Unknown gesture type.");
